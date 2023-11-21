3d478c157d183ed3c37a0676176b19c2eb700bc1
Patricia Alonso Ayllón <alonso.aypat22@triana.salesianos.edu>
2023-11-21 14:27:23 +0100

    holi

--- Saludos/src/saludos/Principal.java
+++ Saludos/src/saludos/Principal.java
@@ -4,6 +4,11 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("-".repeat(50));
+		System.out.println("Holi");
+		System.out.println("-".repeat(50));
+			
+		
 
 	}
 
